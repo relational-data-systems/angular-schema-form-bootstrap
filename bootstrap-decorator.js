600f0c8cfdcf9bb21fe1eb1a34e862ade4f7fdd4
Luke Dutschke <luke@relational.com.au>
2016-08-31 14:39:54 +0930

    Updated default builders for complex validation.

--- bootstrap-decorator.js
+++ bootstrap-decorator.js
@@ -25,6 +25,7 @@ function(decoratorsProvider, sfBuilderProvider, sfPathProvider) {
   var sfField             = sfBuilderProvider.builders.sfField;
   var condition           = sfBuilderProvider.builders.condition;
   var array               = sfBuilderProvider.builders.array;
+  var complexValidation   = sfBuilderProvider.builders.complexValidation;
 
   // Tabs is so bootstrap specific that it stays here.
   var tabs = function(args) {
@@ -45,7 +46,7 @@ function(decoratorsProvider, sfBuilderProvider, sfPathProvider) {
     }
   };
 
-  var defaults = [sfField, ngModel, ngModelOptions, condition];
+  var defaults = [sfField, ngModel, ngModelOptions, condition, complexValidation];
   decoratorsProvider.defineDecorator('bootstrapDecorator', {
     textarea: {template: base + 'textarea.html', builder: defaults},
     fieldset: {template: base + 'fieldset.html', builder: [sfField, simpleTransclusion, condition]},
