f9cc424aacccec4f648b98e9fd9854d898e101d8
Keith Lin <keith@relational.com.au>
2016-09-19 09:41:23 +0930

    abstracting multiple-column mechanism to generic directives

---
+++ src/rds-multi-columns.directive.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('schemaForm')
+        .directive('rdsMultiColumns', rdsMultiColumns);
+
+    /* @ngInject */
+    function rdsMultiColumns($log) {
+        var directive = {
+            scope: false,
+            controller: RdsMultiColumnsController,
+            controllerAs: 'mcCtrl',
+            restrict: 'A',
+            templateUrl: 'decorators/bootstrap/rds-multi-columns.template.html'
+        };
+        return directive;
+    }
+
+    RdsMultiColumnsController.$inject = ['$scope', '$log'];
+
+    /* @ngInject */
+    function RdsMultiColumnsController($scope, $log) {
+      var vm = this;
+
+      vm.bootstrapCol = 12 / $scope.form.columns;
+      // TODO: Can be further abstracted for use in radio, we may need to use an isolated scope
+      vm.itemsPerColumn = parseInt($scope.titleMapValues.length / $scope.form.columns);
+    }
+})();
