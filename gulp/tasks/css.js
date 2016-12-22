5d51807ead080b355ad7ead4d29aa7c7779d4ff6
Keith Lin <keith@relational.com.au>
2016-12-22 11:29:30 +1030

    revise css

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+    cssnano = require('gulp-cssnano');
 
 gulp.task('css', function() {
+  //gulp.src('./src/**/bootstrap-decorator.css')
   gulp.src('./src/**/*.css')
     .pipe(rename({dirname: ''}))
     .pipe(gulp.dest('./'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
+    .pipe(cssnano())
     .pipe(gulp.dest('./'));
 });
